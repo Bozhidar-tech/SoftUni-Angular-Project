f4005c6d88a275f6c31e72af7997cc797e9e6e97
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2024-03-23 00:49:39 +0100

    Removed middleware

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-// const port = 3000;
 dotenv.config();
 
 app.use(cors());
@@ -18,30 +17,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/workout", workoutRoute);
 app.use("/api/product", productRoute);
 
-// Error Handler Middleware
-app.use((err, req, res, next) => {
-    const statusCode = err.status || 500;
-    const errorMessage = err.message || "Something went wrong!";
-    return res.status(statusCode).json({
-        success: false,
-        status: statusCode,
-        message: errorMessage,
-        stack: err.stack
-    })
-});
-
-// Success Handler Middleware
-app.use((obj, req, res, next) => {
-    const statusCode = obj.status || 500;
-    const message = obj.message || "Something went wrong!";
-    return res.status(statusCode).json({
-        success: [200,201,204].some(a=> a === obj.status) ? true : false,
-        status: statusCode,
-        message: message,
-        data: obj.data
-    })
-});
-
 // DB Connection
 const connectMongoDB = async()=>{
     try{
@@ -50,7 +25,6 @@ const connectMongoDB = async()=>{
         throw error;
     }
 }
-
 
 // Start the server
 app.listen(3000, () => {
