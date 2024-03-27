b228edc9b1b048d067b706b6bf80886c27ba3233
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2024-03-27 01:49:11 +0100

    Route Guard added comments

--- gym/src/app/guards/auth-guard.guard.ts
+++ gym/src/app/guards/auth-guard.guard.ts
@@ -2,15 +2,29 @@ import { CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } fr
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+
+  // Inject Router Service
   const router: Router = inject(Router);
+
+  // Define the protected routes
   const protectedRoutes: string[] = ['/dashboard', '/workouts', '/update-data', '/reset/:token', '/update-product/:id', '/my-cart', '/create-product'];
+
+  // Check if the user is logged in
   const session = localStorage.getItem('user_id');
 
+  // Check if the requested route is protected and if the user is logged in
   if (protectedRoutes.includes(state.url) && !session) {
+    
+    // Display alert if the route is protected
     alert('Please login first!');
+
+    // Redirect to the home page
     router.navigate(['/home']);
+
+    // Deny access to the route
     return false;
   }
 
+  // Allow access to the route
   return true;
 };
