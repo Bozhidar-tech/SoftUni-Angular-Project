b57be1d7a57cf894887b100ab1e043cca5f09c8c
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2024-04-03 02:36:12 +0200

    Added Google Cloud Storage for the videos

--- gym/src/app/user/strength-training/strength-training.component.ts
+++ gym/src/app/user/strength-training/strength-training.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Exercise } from 'src/app/types/exercise';
+import { ExerciseService } from 'src/app/shared/exercises.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-strength-training',
   templateUrl: './strength-training.component.html',
   styleUrls: ['./strength-training.component.css']
 })
-export class StrengthTrainingComponent {
+export class StrengthTrainingComponent implements OnInit {
 
+  exercises: Exercise[] = [];
+  errorMessage: string = '';
+
+  constructor(private exerciseService: ExerciseService, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.loadExercises();
+  }
+
+  loadExercises(): void {
+    this.exerciseService.getAllExercises().subscribe({
+      next: (response: any) => {
+        if (response && response.success) {
+          this.exercises = response.data.map((exercise: Exercise) => ({
+            ...exercise,
+            videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl(exercise.videoUrl)
+          })) || [];
+        } else {
+          console.error('Failed to fetch exercises:', response);
+          this.errorMessage = 'Failed to fetch exercises. Please try again later.';
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching exercises:', error);
+        this.errorMessage = 'Failed to fetch exercises. Please try again later.';
+      }
+    });
+  }
 }
