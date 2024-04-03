ff3f8e907e057276d128c3f27d8e0d4d5c429e82
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2024-04-03 03:54:44 +0200

    Small html/css changes

--- gym/src/app/home-components/classes/classes.component.ts
+++ gym/src/app/home-components/classes/classes.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ClassesComponent {
   schedule = [
-    { day: 'morning', classes: [{ name: 'Fit Mix', trainer: 'Lars Wolf', time: '08:00 - 09:30' }, { name: 'Bodytone', trainer: 'Kerstin Ahlefeld', time: '10:00 - 11:30' }] },
-    { day: 'noon', classes: [{ name: 'Step', trainer: 'Ralf Dr Kunkle', time: '12:00 - 13:30' }] },
-    { day: 'afternoon', classes: [{ name: 'Dance Moves', trainer: 'Ralf Dr Kunkle', time: '13:00 - 14:30' }, { name: 'Pilates', trainer: 'Kerstin Ahlefeld', time: '15:00 - 16:30' }, { name: 'Jumping Fitness', trainer: 'Julia Frank', time: '17:00 - 18:00' }] },
+    { day: 'morning', classes: [{ name: 'Boxing', trainer: 'Canelo Alvarez', time: '08:00 - 09:30' }, { name: 'Bodybuilding', trainer: 'Chris Bumstead', time: '10:00 - 11:30' }] },
+    { day: 'noon', classes: [{ name: 'Boxing', trainer: 'Canelo Alvarez', time: '12:00 - 13:30' }] },
+    { day: 'afternoon', classes: [{ name: 'Nutrition Class', trainer: 'Greg Doucette', time: '13:00 - 14:30' }, { name: 'Bodybuilding', trainer: 'Chris Bumstead', time: '15:00 - 16:30' }, { name: 'Boxing', trainer: 'Canelo Alvarez', time: '17:00 - 18:00' }] },
     { day: 'evening', classes: [{ name: 'Nutrition Class', trainer: 'Greg Doucette', time: '18:30 - 19:30' }, { name: 'Boxing', trainer: 'Canelo Alvarez', time: '20:00 - 21:30' }, { name: 'Bodybuilding', trainer: 'Chris Bumstead', time: '22:00 - 23:30' }] },
   ];
 }
