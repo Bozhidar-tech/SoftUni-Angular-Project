5c5d01fd50ee914bcf8d35f3616838edf06f5a63
Bozhidar Nunev <124468291+Bozhidar-tech@users.noreply.github.com>
2024-04-05 19:34:28 +0200

    Added Documentation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,6 +7,7 @@ import { CreateError } from "../utils/error.js";
 import UserToken from "../models/UserToken.js";
 
 export const register = async (req, res, next) => {
+ try {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
@@ -17,7 +18,11 @@ export const register = async (req, res, next) => {
         password: hashPassword,
     });
     await newUser.save();
-    return next(CreateSuccess(200), "User Registered successfully!")
+    return res.status(200).json({ message: "User registered successfully!" });
+} catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+}
 };
 
 export const registerAdmin = async (req, res, next) => {
@@ -74,60 +79,63 @@ export const login = async (req, res, next) => {
 };
 
 export const sendEmail = async (req, res, next) => {
-    const email = req.body.email;
-    const user = await User.findOne({email: {$regex: '^'+email+'$', $options: 'i'}});
+    try {
+        const email = req.body.email;
+        const user = await User.findOne({ email: { $regex: '^' + email + '$', $options: 'i' } });
 
-    if(!user){
-        return next(CreateError(404, "User not found!"));
-    }
-    const payload ={
-        email: user.email
-    }
-    const expiryTime = 300;
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: expiryTime});
-    const newToken = new UserToken({
-        userId: user._id,
-        token: token
-    });
-
-    const mailTransporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "bozhidar.nunev@gmail.com",
-            pass: ""
+        if (!user) {
+            return next(CreateError(404, "User not found!"));
         }
-    });
 
-    let mailDetails = {
-        from: "bozhidar.nunev@abv.bg",
-        to: email,
-        subject: "Reset Password",
-        html: `
-        <html>
-        <head>
-            <title>Password Reset Request</title>
-        </head>
-        <body>
-            <h1>Password Reset Request</h1>
-            <p>Hello ${user.fullName}</p>
-            <p>We have received a request to reset your password for your account with Gold's Gym. To complete the password reset process, please click on the button below:</p>
-            <a href=${process.env.LIVE_URL}/reset/${token}><button>Reset Passoword</button></a>
-            <p>Please note that this link is only valid for 5 minutes</p>
-            <p>If you did not request this, please ignore this email.</p>
-            <p>Thank you</p>
-            <p>Gold's Gym</p>
-        </body>
-        </html>
-        `,
-    };
-    mailTransporter.sendMail(mailDetails, async (err, data)=>{
-        if(err){
-            return next(CreateError(500, "Something went wrong while sending the email!"))
-        }else{
-            await newToken.save();
-            return next(CreateSuccess(200, "Email sent successfuly!"))
-        }
-    })
+        const payload = {
+            email: user.email
+        };
+        const expiryTime = 300;
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: expiryTime });
+        const newToken = new UserToken({
+            userId: user._id,
+            token: token
+        });
+
+        const mailTransporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "bozhidar.nunev@gmail.com",
+                pass: ""
+            }
+        });
+
+        let mailDetails = {
+            from: "bozhidar.nunev@gmail.com",
+            to: email,
+            subject: "Reset Password",
+            html: `
+                <html>
+                <head>
+                    <title>Password Reset Request</title>
+                </head>
+                <body>
+                    <h1>Password Reset Request</h1>
+                    <p>Hello ${user.fullName}</p>
+                    <p>We have received a request to reset your password for your account with Gold's Gym. To complete the password reset process, please click on the button below:</p>
+                    <a href=${process.env.LIVE_URL}/reset/${token}><button>Reset Password</button></a>
+                    <p>Please note that this link is only valid for 5 minutes</p>
+                    <p>If you did not request this, please ignore this email.</p>
+                    <p>Thank you</p>
+                    <p>Gold's Gym</p>
+                </body>
+                </html>
+            `,
+        };
+
+        await mailTransporter.sendMail(mailDetails);
+        await newToken.save();
+        
+        res.status(200).json({ message: "Email sent successfully!" });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        return next(CreateError(500, "Something went wrong while sending the email!"));
+    }
 };
 
 export const resetPassword = async (req, res, next) => {
